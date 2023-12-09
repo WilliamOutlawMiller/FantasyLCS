618a8fd259dbf5a3ac1c4c3cf1ad0368a3e29793
William Miller <williamoutlawmiller@gmail.com>
2023-12-09 11:54:46 -0600

    feat(Constants.cs): Created new constants file and added initial constant for url construction

---
+++ TheHangoutPresentsFantasyLCS/Constants.cs
@@ -0,0 +1,10 @@
+namespace Constants
+{
+    public class SeasonInfo
+    {
+        // This is used for url construction. The lol.fandom.com url follows this format:
+        // https://lol.fandom.com/wiki/LCS/2023_Season/Spring_Season/
+        public const string SEASON_YEAR = "2023_Season";
+        
+    }
+}
