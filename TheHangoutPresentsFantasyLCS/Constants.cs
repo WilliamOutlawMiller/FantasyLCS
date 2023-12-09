c34b006a07f6d761bcce721c12aeb09fddcac5a8
William Miller <williamoutlawmiller@gmail.com>
2023-12-09 11:55:42 -0600

    feat(Constants.cs): added domain wiki

--- TheHangoutPresentsFantasyLCS/Constants.cs
+++ TheHangoutPresentsFantasyLCS/Constants.cs
@@ -2,9 +2,11 @@ namespace Constants
 {
     public class SeasonInfo
     {
-        // This is used for url construction. The lol.fandom.com url follows this format:
+        // These constants are used for url construction. The lol.fandom.com url follows this format:
         // https://lol.fandom.com/wiki/LCS/2023_Season/Spring_Season/
+
+        public const string DOMAIN = "https://lol.fandom.com/wiki/LCS/";
         public const string SEASON_YEAR = "2023_Season";
-        
+
     }
 }
