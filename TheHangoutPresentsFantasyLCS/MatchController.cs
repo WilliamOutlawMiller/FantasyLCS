6aedc14f5e56b0aebd5a39b92b9f07757e40ac10
William Miller <williamoutlawmiller@gmail.com>
2023-12-09 15:03:54 -0600

    feat(MatchController.cs): CreatedParseHtmlToDataTable to take an html table and parse it into a datatable

--- TheHangoutPresentsFantasyLCS/MatchController.cs
+++ TheHangoutPresentsFantasyLCS/MatchController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Text;
 using System.IO;
+using System.Data;
 using Microsoft.AspNetCore.Mvc;
 
 public class MatchController : Controller
@@ -14,9 +15,8 @@ public class MatchController : Controller
         string picksAndBansXPath = "//table[@class='wikitable plainlinks hoverable-rows column-show-hide-1' and @id='pbh-table']/tbody";
         try
         {
-            string htmlTable = ParseHtml(url, picksAndBansXPath).Result;
-            // todo: parse html into readable datatable or other data structure
-            return View();
+            HtmlNode tableNode = LocateHTMLNode(url, picksAndBansXPath).Result;
+            DataTable dataTable = ParseHtmlToDataTable(tableNode);
         }
         catch
         {
@@ -24,7 +24,7 @@ public class MatchController : Controller
         }
     }
 
-    private static async Task<string> ParseHtml(string url, string xpath)
+    private static async Task<HtmlNode> LocateHTMLNode(string url, string xpath)
     {
         var web = new HtmlWeb();
         var doc = await web.LoadFromWebAsync(url);
@@ -35,7 +35,7 @@ public class MatchController : Controller
         {
             if (tbodyElement != null)
             {
-                return tbodyElement.OuterHtml;
+                return tbodyElement;
             }
             else
             {
@@ -44,7 +44,59 @@ public class MatchController : Controller
         }
         catch (Exception ex)
         {
-            return await Task.FromException<string>(ex);
+            return await Task.FromException<HtmlNode>(ex);
         }
     }
+
+    private static DataTable ParseHtmlToDataTable(HtmlNode tableNode)
+    {
+        var dataTable = new DataTable();
+        
+        // Skip the title row
+        var tableRows = tableNode.Descendants("tr").Skip(1);
+
+        // Extract column headers
+        var headerRow = tableRows.FirstOrDefault();
+        var headers = headerRow.Descendants("th");
+        foreach (var header in headers)
+        {
+            dataTable.Columns.Add(header.InnerText.Trim());
+        }
+
+        // Skip the header row
+        var dataRows = tableRows.Skip(1); 
+
+        // Extract rows and data
+        foreach (var row in dataRows) // Skip the header row
+        {
+            var dataCells = row.Descendants("td").ToArray();
+            var rowData = new List<string>();
+
+            foreach (var cell in dataCells)
+            {
+                var titleAttribute = cell.Attributes["title"];
+                var championAttribute = cell.Attributes["data-c1"];
+                if (championAttribute != null)
+                {
+                    // If a title attribute is present, use its value
+                    rowData.Add(championAttribute.Value.Trim());
+                }
+                else if (titleAttribute != null)
+                {
+                    // If a title attribute is present, use its value
+                    rowData.Add(titleAttribute.Value.Trim());
+                }
+                else
+                {
+                    // Otherwise, use the inner text
+                    rowData.Add(cell.InnerText.Trim());
+                }
+            }
+
+            dataTable.Rows.Add(rowData.ToArray());
+        }
+
+        return dataTable;
+    }
+
 }
