a23e2fee55f5fbda9f39d20809cf309768f89667
RichardLy <richardly527@gmail.com>
2023-12-09 19:10:24 -0600

    chore(PickBans.cs): Included model names for Pick Ban table

--- TheHangoutPresentsFantasyLCS/DataObjects/PickBans.cs
+++ TheHangoutPresentsFantasyLCS/DataObjects/PickBans.cs
@@ -19,5 +19,41 @@ public class PickBans
 
         // Lists the patch that the game was played on. Technically a float, but string is fine too.
         public string Patch { get; set; }
+
+        public string BB1 { get; set; }
+
+        public string RB1 { get; set; }
+
+        public string BB2 { get; set; }
+
+        public string RB2 { get; set; }
+
+        public string BB3 { get; set; }
+
+        public string RB3 { get; set; }
+
+        public string BP1 { get; set; }
+
+        public string RP1_2 { get; set; }
+
+        public string BP2_3 { get; set; }
+
+        public string RP3 { get; set; }
+
+        public string RB4 { get; set; }
+
+        public string BB4 { get; set; }
+
+        public string RB5 { get; set; }
+
+        public string BB5 { get; set; }
+
+        public string RP4 { get; set; }
+
+        public string BP4_5 { get; set; }
+
+        public string SB { get; set; }
+
+        public string VOD { get; set; }
     }
 }
