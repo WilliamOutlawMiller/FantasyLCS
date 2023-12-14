505175a18314eeacb17dea8ac24d2dff25b3e42b
William Miller <williamoutlawmiller@gmail.com>
2023-12-13 19:20:40 -0600

    fix(Constants.cs): Fixed url construction

--- TheHangoutPresentsFantasyLCS/Constants.cs
+++ TheHangoutPresentsFantasyLCS/Constants.cs
@@ -1,3 +1,5 @@
+using HtmlAgilityPack;
+
 namespace Constants
 {
     public class SeasonInfo
@@ -5,9 +7,9 @@ namespace Constants
         // These constants are used for url construction. The lol.fandom.com url follows this format:
         // https://lol.fandom.com/wiki/LCS/2023_Season/Spring_Season/
 
-        public const string DOMAIN = "https://gol.gg/tournament/tournament-matchlist";
-        public const string SEASON = "/LCS%20Spring%20";
-        public const string YEAR = "2023";
+        public const string DOMAIN = "https://gol.gg/tournament/tournament-matchlist/";
+        public const string SEASON = "LCS%20Spring%20";
+        public const string YEAR = "2023/";
     }
 
     public class MatchInfo
