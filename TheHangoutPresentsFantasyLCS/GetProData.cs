ff13bede0b5fd738a072c2ea67dd2a4d612898db
William Miller <williamoutlawmiller@gmail.com>
2023-12-18 00:10:28 -0600

    chore(GetProData.cs): todo

--- TheHangoutPresentsFantasyLCS/GetProData.cs
+++ TheHangoutPresentsFantasyLCS/GetProData.cs
@@ -6,6 +6,8 @@ public class GetProData
 {
     public static Match GetMatch()
     {
+        // todo: right now this is reading the fullstats page of one game. This will need to be refactored for BO5 that reads from multiple matches
+        
         Match match = new Match();
         // string url = SeasonInfo.DOMAIN + SeasonInfo.SEASON + SeasonInfo.YEAR; 
         string url = "https://gol.gg/game/stats/53263/page-fullstats/";
