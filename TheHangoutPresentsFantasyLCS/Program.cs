2aa0550f26effb94ac5c693881623125c54cbf04
William Miller <williamoutlawmiller@gmail.com>
2023-12-21 10:56:44 -0600

    chore(Program.cs): Removed comment

--- TheHangoutPresentsFantasyLCS/Program.cs
+++ TheHangoutPresentsFantasyLCS/Program.cs
@@ -70,9 +70,3 @@ app.MapGet("/getplayer/{id}", (int id) =>
 .WithOpenApi();
 
 app.Run();
-
-
-// I am removing the API, as it feels unnecessary at this point. 
-// The data returned from these two methods is far too big to be returned at an endpoint,
-// so we might as well just stick to file IO until necessary.
-
