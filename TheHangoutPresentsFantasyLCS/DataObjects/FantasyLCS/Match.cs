5fc84cbf44b587dd31087d9efc3ccabc7a79f71a
William Miller <williamoutlawmiller@gmail.com>
2023-12-20 21:01:27 -0600

    chore(Match.cs): changed the match object to have useful properties. Will probably have to change them again

--- TheHangoutPresentsFantasyLCS/DataObjects/FantasyLCS/Match.cs
+++ TheHangoutPresentsFantasyLCS/DataObjects/FantasyLCS/Match.cs
@@ -1,14 +1,6 @@
 public class Match
 {
     public int ID { get; set; }
-    public Team TeamOne { get; set; }
-    public Team TeamTwo { get; set; }
-    
-    // todo: commented out until we have winner and loser data from page other than fullstats
-    /* 
-    public Team Winner { get; set; }
-    public Team Loser { get; set; }
-    */
-
+    public List<Player> Players { get; set; }
     public List<FullStats> FullStats { get; set; }
 }
