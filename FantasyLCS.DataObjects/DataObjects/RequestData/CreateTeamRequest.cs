e38dd8124662d49b3132b1ba3f9532193c9514c9
William Miller <williamoutlawmiller@gmail.com>
2023-12-23 17:37:09 -0600

    fix(CreateTeamRequest.cs): added json property names for deserialization

--- FantasyLCS.DataObjects/DataObjects/RequestData/CreateTeamRequest.cs
+++ FantasyLCS.DataObjects/DataObjects/RequestData/CreateTeamRequest.cs
@@ -1,6 +1,11 @@
-﻿public class CreateTeamRequest
+﻿using System.Text.Json.Serialization;
+
+public class CreateTeamRequest
 {
+    [JsonPropertyName("name")]
     public string Name { get; set; }
+    [JsonPropertyName("username")]
     public string Username { get; set; }
+    [JsonPropertyName("logoUrl")]
     public string LogoUrl { get; set; }
 }
