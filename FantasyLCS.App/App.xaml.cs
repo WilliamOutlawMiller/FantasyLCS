8ef47bf147a2ac4086e02c429e04922def2d5ac0
William Miller <williamoutlawmiller@gmail.com>
2023-12-23 14:02:26 -0600

    feat(App.xaml.cs): Updated to initialize apiservice and mainviewmodel on startup

--- FantasyLCS.App/App.xaml.cs
+++ FantasyLCS.App/App.xaml.cs
@@ -1,14 +1,22 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System.Net.Http;
 using System.Windows;
 
 namespace FantasyLCS.App
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
 
+            var apiService = new ApiService(new HttpClient(), "your_api_base_url");
+            var mainViewModel = new MainViewModel(apiService);
+
+            var mainWindow = new MainWindow
+            {
+                DataContext = mainViewModel
+            };
+            mainWindow.Show();
+        }
+    }
 }
