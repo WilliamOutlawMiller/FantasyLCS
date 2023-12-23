f39e9370687aa94c1ff83892cddc0c5b4dc8e8b7
William Miller <williamoutlawmiller@gmail.com>
2023-12-23 14:02:07 -0600

    feat(ApiService.cs): Updated to include CreateTeam method and getter methods

--- FantasyLCS.App/ApiService.cs
+++ FantasyLCS.App/ApiService.cs
@@ -1,25 +1,91 @@
 ﻿using FantasyLCS.DataObjects;
+using System;
 using System.Collections.ObjectModel;
 using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 public class ApiService
 {
     private readonly HttpClient _httpClient;
+    private readonly string _baseApiUrl; // Base URL of your API
 
-    public ApiService(HttpClient httpClient)
+    public ApiService(HttpClient httpClient, string baseApiUrl)
     {
         _httpClient = httpClient;
+        _baseApiUrl = baseApiUrl;
     }
 
     public async Task<ObservableCollection<Player>> LoadAvailablePlayersAsync()
     {
-        // Logic to call the API and get available players
-        // Deserialize the response and return the players collection
+        try
+        {
+            var response = await _httpClient.GetAsync($"{_baseApiUrl}/getavailableplayers");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<ObservableCollection<Player>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            }
+            // Handle not found or other unsuccessful status codes
+        }
+        catch (Exception ex)
+        {
+            // Handle exceptions
+        }
+        return new ObservableCollection<Player>();
     }
 
     public async Task<ObservableCollection<Team>> LoadTeamsAsync()
     {
-        // Logic to call the API and get teams
-        // Deserialize the response and return the teams collection
+        try
+        {
+            var response = await _httpClient.GetAsync($"{_baseApiUrl}/getallteams");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<ObservableCollection<Team>>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            }
+            // Handle not found or other unsuccessful status codes
+        }
+        catch (Exception ex)
+        {
+            // Handle exceptions
+        }
+        return new ObservableCollection<Team>();
+    }
+
+    public async Task<bool> CreateTeamAsync(string name, string username)
+    {
+        try
+        {
+            // Create an object to send in the request body
+            var requestData = new { name, username };
+
+            // Serialize the requestData to JSON
+            var jsonRequest = JsonSerializer.Serialize(requestData);
+
+            // Create a StringContent from the JSON data
+            var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+            // Send a POST request to the /createteam/{name}/{username} endpoint
+            var response = await _httpClient.PostAsync($"/createteam/{name}/{username}", content);
+
+            // Check if the request was successful (status code 200)
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                // Handle the error here if needed
+                return false;
+            }
+        }
+        catch (Exception ex)
+        {
+            // Handle exceptions here if needed
+            return false;
+        }
     }
 }
