9783fd087e99754a58f47c07de5fcb61d0422321
William Miller <williamoutlawmiller@gmail.com>
2023-12-23 18:03:42 -0600

    chore(Team.cs): Added path reference to Team to find logo

--- FantasyLCS.DataObjects/DataObjects/FantasyLCS/Team.cs
+++ FantasyLCS.DataObjects/DataObjects/FantasyLCS/Team.cs
@@ -6,6 +6,7 @@ namespace FantasyLCS.DataObjects
         public string Name { get; set; }
         public string OwnerName { get; set; }
         public string LogoUrl { get; set; }
+        public string LogoPath { get; set; }
         public int Wins { get; set; }
         public int Losses { get; set; }
         public List<int> PlayerIDs { get; set; }
