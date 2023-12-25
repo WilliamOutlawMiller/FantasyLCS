3e937a9da8fdcc50103c6ac71049767f3ea3c54e
William Miller <williamoutlawmiller@gmail.com>
2023-12-25 03:28:03 -0600

    feat(FantasyLCS): Started new .NET MAUI app to replace the FantasyLCS.App WPF project

---
+++ FantasyLCS/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FantasyLCS
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
