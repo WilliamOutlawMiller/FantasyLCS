ef5c2799d6c34b6f78d118a24525cc76483cd197
William Miller <williamoutlawmiller@gmail.com>
2023-12-26 12:16:40 -0600

    chore(FantasyLCS.sln): Deleted project, will reinit with template

--- FantasyLCS/MVVM/ViewModels/MainViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace FantasyLCS.MVVM.ViewModels
-{
-    internal class MainViewModel : ObservableObject
-    {
-
-    }
-}
--- FantasyLCS/MVVM/Views/LoginPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace FantasyLCS.MVVM.Views;
-
-public partial class LoginPage : ContentPage
-{
-	public LoginPage()
-	{
-		InitializeComponent();
-	}
-}
