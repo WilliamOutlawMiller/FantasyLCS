0d212864accda1ccd7c29e028c18f421c0ccd42e
William Miller <williamoutlawmiller@gmail.com>
2024-01-20 01:08:25 -0600

    feat(MatchScheduler): Created match scheduling functionality

---
+++ FantasyLCS.API/Migrations/20240120065827_RemoveMatchIDFromLeagueMatch.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FantasyLCS.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveMatchIDFromLeagueMatch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LeagueMatches_Matches_MatchID",
+                table: "LeagueMatches");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LeagueMatches_MatchID",
+                table: "LeagueMatches");
+
+            migrationBuilder.DropColumn(
+                name: "MatchID",
+                table: "LeagueMatches");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MatchID",
+                table: "LeagueMatches",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LeagueMatches_MatchID",
+                table: "LeagueMatches",
+                column: "MatchID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LeagueMatches_Matches_MatchID",
+                table: "LeagueMatches",
+                column: "MatchID",
+                principalTable: "Matches",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
